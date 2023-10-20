abbe5afaea5c98a6d58062a297c930ed3a6f4e39
jhona2004 <jonathan.pena51@unach.mx>
2023-10-20 16:40:57 -0600

    Agregar archivos de proyecto.

---
+++ Formulario_Prueba/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Formulario_Prueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNombre = new System.Windows.Forms.Label();
+            this.tbApellidos = new System.Windows.Forms.Label();
+            this.tbTelefono = new System.Windows.Forms.Label();
+            this.tbEstatura = new System.Windows.Forms.Label();
+            this.tbEdad = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.Apellidos = new System.Windows.Forms.TextBox();
+            this.Telefono = new System.Windows.Forms.TextBox();
+            this.Estatura = new System.Windows.Forms.TextBox();
+            this.Edad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbMujer = new System.Windows.Forms.RadioButton();
+            this.rbHombre = new System.Windows.Forms.RadioButton();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.AutoSize = true;
+            this.tbNombre.Location = new System.Drawing.Point(50, 51);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(56, 16);
+            this.tbNombre.TabIndex = 0;
+            this.tbNombre.Text = "Nombre";
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.AutoSize = true;
+            this.tbApellidos.Location = new System.Drawing.Point(50, 92);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(64, 16);
+            this.tbApellidos.TabIndex = 1;
+            this.tbApellidos.Text = "Apellidos";
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.AutoSize = true;
+            this.tbTelefono.Location = new System.Drawing.Point(50, 127);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(61, 16);
+            this.tbTelefono.TabIndex = 2;
+            this.tbTelefono.Text = "Telefono";
+            // 
+            // tbEstatura
+            // 
+            this.tbEstatura.AutoSize = true;
+            this.tbEstatura.Location = new System.Drawing.Point(50, 162);
+            this.tbEstatura.Name = "tbEstatura";
+            this.tbEstatura.Size = new System.Drawing.Size(56, 16);
+            this.tbEstatura.TabIndex = 3;
+            this.tbEstatura.Text = "Estatura";
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.AutoSize = true;
+            this.tbEdad.Location = new System.Drawing.Point(50, 201);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(40, 16);
+            this.tbEdad.TabIndex = 4;
+            this.tbEdad.Text = "Edad";
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(130, 51);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(198, 22);
+            this.Nombre.TabIndex = 5;
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.Location = new System.Drawing.Point(130, 92);
+            this.Apellidos.Name = "Apellidos";
+            this.Apellidos.Size = new System.Drawing.Size(198, 22);
+            this.Apellidos.TabIndex = 6;
+            // 
+            // Telefono
+            // 
+            this.Telefono.Location = new System.Drawing.Point(130, 127);
+            this.Telefono.Name = "Telefono";
+            this.Telefono.Size = new System.Drawing.Size(198, 22);
+            this.Telefono.TabIndex = 7;
+            // 
+            // Estatura
+            // 
+            this.Estatura.Location = new System.Drawing.Point(130, 162);
+            this.Estatura.Name = "Estatura";
+            this.Estatura.Size = new System.Drawing.Size(198, 22);
+            this.Estatura.TabIndex = 8;
+            // 
+            // Edad
+            // 
+            this.Edad.Location = new System.Drawing.Point(130, 201);
+            this.Edad.Name = "Edad";
+            this.Edad.Size = new System.Drawing.Size(198, 22);
+            this.Edad.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbMujer);
+            this.groupBox1.Controls.Add(this.rbHombre);
+            this.groupBox1.Location = new System.Drawing.Point(102, 259);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 86);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Genero";
+            // 
+            // rbMujer
+            // 
+            this.rbMujer.AutoSize = true;
+            this.rbMujer.Location = new System.Drawing.Point(135, 46);
+            this.rbMujer.Name = "rbMujer";
+            this.rbMujer.Size = new System.Drawing.Size(61, 20);
+            this.rbMujer.TabIndex = 1;
+            this.rbMujer.TabStop = true;
+            this.rbMujer.Text = "Mujer";
+            this.rbMujer.UseVisualStyleBackColor = true;
+            // 
+            // rbHombre
+            // 
+            this.rbHombre.AutoSize = true;
+            this.rbHombre.Location = new System.Drawing.Point(6, 46);
+            this.rbHombre.Name = "rbHombre";
+            this.rbHombre.Size = new System.Drawing.Size(77, 20);
+            this.rbHombre.TabIndex = 0;
+            this.rbHombre.TabStop = true;
+            this.rbHombre.Text = "Hombre";
+            this.rbHombre.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(81, 383);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(120, 46);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(228, 383);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(120, 46);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 561);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Edad);
+            this.Controls.Add(this.Estatura);
+            this.Controls.Add(this.Telefono);
+            this.Controls.Add(this.Apellidos);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbEstatura);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.tbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tbNombre;
+        private System.Windows.Forms.Label tbApellidos;
+        private System.Windows.Forms.Label tbTelefono;
+        private System.Windows.Forms.Label tbEstatura;
+        private System.Windows.Forms.Label tbEdad;
+        private System.Windows.Forms.TextBox Nombre;
+        private System.Windows.Forms.TextBox Apellidos;
+        private System.Windows.Forms.TextBox Telefono;
+        private System.Windows.Forms.TextBox Estatura;
+        private System.Windows.Forms.TextBox Edad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbMujer;
+        private System.Windows.Forms.RadioButton rbHombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Formulario_Prueba/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient; //Referencia necesaria para trabajar con localhost y XAMPP
+using System.Text.RegularExpressions;
+
+namespace Formulario_Prueba
+{
+    public partial class Form1 : Form
+    {
+        //Datos de conexión a MySql (Xampp)
+        string conexionSQL = "Server=localhost;Port=3306;Database=programacionavanzada;Uid=root;Pwd=;";
+        //Metodo de para insertar registros
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tbEdad.TextChanged += ValidarEdad;
+            tbEstatura.TextChanged += ValidarEstatura;
+            tbTelefono.TextChanged += ValidarTelefono;
+            tbNombre.TextChanged += ValidarNombre;
+            tbApellidos.TextChanged += ValidarApellidos;
+        }
+
+        private void InsertarRegistro(string nombre, string apellidos, int edad, decimal estatura, string telefono, string genero)
+        {
+            using (MySqlConnection conection = new MySqlConnection(conexionSQL))
+            {
+                conection.Open();
+
+                string insertQuery = "INSERT INTO registro (Nombre, Apellidos, Edad, Estatura, Telefono, Genero)" +
+                                     "VALUES (@Nombre, @Apellidos, @Edad, @Estatura, @Telefono, @Genero)";
+
+                using (MySqlCommand command = new MySqlCommand(insertQuery, conection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Apellidos", apellidos);
+                    command.Parameters.AddWithValue("@Edad", edad);
+                    command.Parameters.AddWithValue("@Estatura", estatura);
+                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@Genero", genero);
+
+                    command.ExecuteNonQuery();
+                }
+                conection.Close();
+            }
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Obtener los datos de los TexBox
+            string nombres = tbNombre.Text;
+            string apellidos = tbApellidos.Text;
+            string edad = tbEdad.Text;
+            string estatura = tbEstatura.Text;
+            string telefono = tbTelefono.Text;
+
+
+            //Obtener el genero seleccinado
+            string genero = "";
+            if (rbHombre.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (rbMujer.Checked)
+            {
+                genero = "Mujer";
+            }
+            if (EsEnteroValido(edad) && EsDecimalValido(estatura) && EsEnteroValidoDe10Digitos(telefono) &&
+                EstextoValido(nombres) && EstextoValido(apellidos))
+            {
+                //Crear una cadena con los datos 
+                string datos = $"Nombres: {nombres}\r\nApellidos: {apellidos}\r\nTelefono: {telefono} kg\r\nEstatura: {estatura}cm\r\nEdad: {edad} años\n\nGenero: {genero}";
+
+                //Guardar los datos en un archivo de texto
+                string rutaArchivo = "C:/Users/Jonat/Downloads.txt";
+                bool archivoExiste = File.Exists(rutaArchivo);
+                if (archivoExiste == false)
+                {
+                    File.WriteAllText(rutaArchivo, datos);
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(rutaArchivo, true))
+                    {
+                        if (archivoExiste)
+                        {
+                            // Si el archivo existe, añadir un separador antes del nuevo regristro 
+                            writer.WriteLine();
+                            //Promacion de la funcionalidad en insert SQL
+                            InsertarRegistro(nombres, apellidos, int.Parse(edad), decimal.Parse(estatura), telefono, genero);
+                            MessageBox.Show("Datos ingresados correctamente.");
+                        }
+                        else
+                        {
+                            writer.WriteLine(datos);
+                            //Programacion de la funcionalidad de insert SQL
+                            InsertarRegistro(nombres, apellidos, int.Parse(edad), decimal.Parse(estatura), telefono, genero);
+                            MessageBox.Show("Datos ingresados correctamente.");
+                        }
+                    }
+                }
+
+                //Mostrar un mensaje con los datos capturados
+                MessageBox.Show("Datos guardados con exito:\n\n" + datos, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese datos validos en los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private bool EsEnteroValido(string valor)
+        {
+            int resultado;
+            return int.TryParse(valor, out resultado);
+        }
+        private bool EsDecimalValido(string valor)
+        {
+            decimal resultado;
+            return decimal.TryParse(valor, out resultado);
+        }
+        private bool EsEnteroValidoDe10Digitos(string input)
+        {
+            if (input.Length != 10)
+            {
+                return false;
+            }
+            if (!input.All(char.IsDigit))
+            {
+                return false;
+            }
+            return true;
+        }
+        private bool EstextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");
+        }
+        private void ValidarEdad(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsEnteroValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese una edad válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarEstatura(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsDecimalValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese una estatura válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarTelefono(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string input = textBox.Text;
+            if (input.Length > 10)
+            {
+                if (!EsEnteroValidoDe10Digitos(input))
+                {
+                    MessageBox.Show("Por favor ingrese un número de teléfono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Clear();
+                }
+            }
+            else if (!EsEnteroValidoDe10Digitos(input))
+            {
+                MessageBox.Show("Por favor ingrese un número de teléfono válido de 10 dígitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ValidarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EstextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un valor válido (solo letras y espacios).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarApellidos(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EstextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese apellidos válidos (solo letras y espacios).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Nombre.Clear();
+            Apellidos.Clear();
+            Telefono.Clear();
+            Estatura.Clear();
+            Edad.Clear();
+            rbHombre.Checked = false;
+            rbMujer.Checked = false;
+        }
+    }
+}
